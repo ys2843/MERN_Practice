1d71910b9c39c8d84e3d83c6baaa238dce1a8fdb
ysh <yang.shi@vudu.com>
2019-10-21 13:04:38 -0700

    initial

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = new express();
+const PORT = process.env.port || 5000;
+
+app.get('/', (req, res) => { res.send(`Server running on port ${PORT}`) });
+app.listen(PORT, () => { console.log(`Server listening to port ${PORT}`) });

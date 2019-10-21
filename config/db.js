a2ae886abab6d49579bd6548f55f5af69c75a70f
ysh <yang.shi@vudu.com>
2019-10-21 14:15:46 -0700

    connect to mongoDB

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoos = require('mongoose');
+const config = require('config');
+const DBUrl = config.get('mongoURL');
+console.log(DBUrl)
+const connectDB = async () => {
+    try {
+        await mongoos.connect(DBUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("connected to database");
+    } catch (e) {
+        console.error(e.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = new express();
 const PORT = process.env.port || 5000;
+const connectDB = require('./config/db');
 
+connectDB();
 app.get('/', (req, res) => { res.send(`Server running on port ${PORT}`) });
 app.listen(PORT, () => { console.log(`Server listening to port ${PORT}`) });
